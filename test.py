d149ea347baf689795c4fb35379760b96caa2e9d
ceilmechanics <wendy.personal.1213@gmail.com>
2025-03-13 15:39:13 -0400

    clean up the repo

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-response_main = requests.get("https://shy-moyna-wendanj-b5959963.koyeb.app/")
-print('Web Application Response:\n', response_main.text, '\n\n')
-
-
-data = {"text":"talk about Policy on Parental Accommodation for Ph.D. Students at tufts"}
-response_llmproxy = requests.post("https://shy-moyna-wendanj-b5959963.koyeb.app/query", json=data)
-print('LLMProxy Response:\n', response_llmproxy.text)
--- test2.py
+++
@@ -1,6 +0,0 @@
-from llmproxy import generate, TuftsCSAdvisor
-
-response = user_advisors[user_id]["advisor"].get_response(
-            query=message, 
-            lastk=current_lastk
-        )
