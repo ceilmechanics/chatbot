c8bdd89851b0469b1d40d2ad0f595b7d42748058
ceilmechanics <wendy.personal.1213@gmail.com>
2025-04-30 16:25:48 -0400

    revise session id

--- advisor.py
+++ advisor.py
@@ -20,7 +20,7 @@ class TuftsCSAdvisor:
             query=query,
             temperature=0.1,
             lastk=0,
-            session_id='cs-advising-handbooks-v5-' + self.user_id,      # prev v5
+            session_id='cs-advising-handbooks-v10-' + self.user_id,      # prev v5
             rag_usage=True,
             rag_threshold=0.5,  # Lower threshold
             rag_k=5  # Retrieve more documents
@@ -42,7 +42,7 @@ class TuftsCSAdvisor:
             query=query,
             temperature=0.1,
             lastk=self.last_k,
-            session_id='cs-advising-handbooks-v5-' + self.user_id,      # prev v5
+            session_id='cs-advising-handbooks-v10-' + self.user_id,      # prev v5
             rag_usage=True,
             rag_threshold=0.5,  # Lower threshold
             rag_k=5  # Retrieve more documents
